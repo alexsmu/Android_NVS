3a6f5b83ca0a3be50e2db349279e4460fe2066a3
alexsmu <alexsmu@gmail.com>
2016-05-23 02:32:04 -0600

    Added listeners and values for testing.

--- app/src/main/java/byuie499/auto_nvs/TabFragment1.java
+++ app/src/main/java/byuie499/auto_nvs/TabFragment1.java
@@ -5,13 +5,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class TabFragment1 extends Fragment {
-    View mMain;
+    private View mMain;
+    private Tab1Data staticData = null;
+    private ToggleButton noise, vibration;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMain = inflater.inflate(R.layout.tab_fragment_1, container, false);
+        staticData = new Tab1Data(mMain.getContext());
+        noise = (ToggleButton) mMain.findViewById(R.id.toggleNoise);
+        vibration = (ToggleButton) mMain.findViewById(R.id.toggleVibration);
+        noise.setChecked(Tab1Data.getBool("Noise", false));
+        vibration.setChecked(Tab1Data.getBool("Vibration", true));
+        noise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Tab1Data.getBool("Noise", false))
+                {
+                    noise.setChecked(false);
+                    Tab1Data.setCheckBox("Noise", false);
+                } else {
+                    noise.setChecked(true);
+                    Tab1Data.setCheckBox("Noise", true);
+                }
+
+            }
+        });
+        vibration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Tab1Data.getBool("Vibration", true))
+                {
+                    vibration.setChecked(false);
+                    Tab1Data.setCheckBox("Vibration", false);
+                } else {
+                    vibration.setChecked(true);
+                    Tab1Data.setCheckBox("Vibration", true);
+                }
+            }
+        });
 
         return mMain;
     }
@@ -26,25 +62,6 @@ public class TabFragment1 extends Fragment {
     public void onResume() {
         super.onResume();
 
-    }
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-
-        // Is fragment currently visible ?
-        if (this.isVisible()) {
-            // Is it becoming invisible?
-            if (!isVisibleToUser) {
-
-            }
-        }
-        else { // Is fragment currently invisible?
-            // Is it becoming visible?
-            if (isVisibleToUser) {
-
-            }
-        }
     }
 
 }
