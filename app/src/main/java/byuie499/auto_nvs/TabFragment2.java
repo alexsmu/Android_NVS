45f35d3fcf0f9f453e15b509aeb800216d610f55
alexsmu <alexsmu@gmail.com>
2016-05-23 02:32:29 -0600

    Added listeners and values for testing.

--- app/src/main/java/byuie499/auto_nvs/TabFragment2.java
+++ app/src/main/java/byuie499/auto_nvs/TabFragment2.java
@@ -1,5 +1,6 @@
 package byuie499.auto_nvs;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -16,6 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +32,16 @@ public class TabFragment2 extends Fragment {
     private TextView textTitle;
     private Spinner fileSpinner;
     private SeekBar fftseekBar;
+    private double[] audio_omega = new double[16384];
+    private double[] accel_omega = new double[256];
+    private Fft[] accelFFT = new Fft[3];
+    private MicData rec_mic = null;
+    private Xlo rec_acc = null;
+    private LineGraphSeries<DataPoint> audioSeries = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> xSeries = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> ySeries = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> zSeries = new LineGraphSeries<>();
+    private GraphView graph = null;
     public Handler mHandler = null;
 
     @Override
@@ -55,29 +71,71 @@ public class TabFragment2 extends Fragment {
                     case 1: // Audio buffer is ready
                     {
                         // begin audio fft
+
                         break;
                     }
                     case 2: // Audio fft is complete
                     {
                         // add to series
+                        double[] result = (double[]) msg.obj;
+                        DataPoint[] dps = new DataPoint[186];
+                        int j = 0;
+                        for (int i = 8192; i < 8378; ++i) {
+                            dps[j++] = new DataPoint(audio_omega[i], result[i]);
+                        }
+                        audioSeries.resetData(dps);
                         break;
                     }
                     case 3: // Accelerometer data is ready
                     {
                         // begin fft
+                        accelFFT[0].run(Xlo.xAcc, "x");
+                        accelFFT[1].run(Xlo.yAcc, "y");
+                        accelFFT[2].run(Xlo.zAcc, "z");
                         break;
                     }
-                    case 4: // Accelerometer fft is complete
+                    case 4: // Accelerometer x fft is complete
+                    {
+                        // add to series
+                        double[] result = (double[]) msg.obj;
+                        DataPoint[] dps = new DataPoint[128];
+                        int j = 0;
+                        for (int i = 128; i < 256; ++i) {
+                            dps[j++] = new DataPoint(accel_omega[i], result[i]);
+                        }
+                        xSeries.resetData(dps);
+                        break;
+                    }
+                    case 5: // Accelerometer y fft complete
+                    {
+                        // add to series
+                        double[] result = (double[]) msg.obj;
+                        DataPoint[] dps = new DataPoint[128];
+                        int j = 0;
+                        for (int i = 128; i < 256; ++i) {
+                            dps[j++] = new DataPoint(accel_omega[i], result[i]);
+                        }
+                        ySeries.resetData(dps);
+                        break;
+                    }
+                    case 6: // Accelerometer z fft complete
+                    {
+                        //add to series
+                        double[] result = (double[]) msg.obj;
+                        DataPoint[] dps = new DataPoint[128];
+                        int j = 0;
+                        for (int i = 128; i < 256; ++i) {
+                            dps[j++] = new DataPoint(accel_omega[i], result[i]);
+                        }
+                        zSeries.resetData(dps);
+                        break;
+                    }
+                    case 7: // Audio fft correlation complete
                     {
                         // add to series
                         break;
                     }
-                    case 5: // Audio fft correlation complete
-                    {
-                        // add to series
-                        break;
-                    }
-                    case 6: // Accelerometer fft correlation complete
+                    case 8: // Accelerometer fft correlation complete
                     {
                         //add to series
                         break;
@@ -90,6 +148,34 @@ public class TabFragment2 extends Fragment {
             }
         };
 
+        rec_acc = new Xlo(this.getActivity(), mHandler, 256, 2);
+        rec_mic = new MicData(mHandler, 16384);
+
+        accelFFT[0] = new Fft(256, mHandler, 4);
+        accelFFT[1] = new Fft(256, mHandler, 5);
+        accelFFT[2] = new Fft(256, mHandler, 6);
+        Fft.getOmega(audio_omega, 44100);
+        Fft.getOmega(accel_omega, 1000);
+        graph = (GraphView) mMain.findViewById(R.id.fftGraph);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(500);
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-60);
+        graph.getViewport().setMaxY(20);
+
+        audioSeries.setTitle("Mic");
+        audioSeries.setColor(Color.BLACK);
+        xSeries.setTitle("X");
+        ySeries.setTitle("Y");
+        zSeries.setTitle("Z");
+        xSeries.setColor(Color.BLUE);
+        ySeries.setColor(Color.GREEN);
+        zSeries.setColor(Color.RED);
 
         return mMain;
     }
@@ -162,6 +248,8 @@ public class TabFragment2 extends Fragment {
                 {
                     Toast.makeText(getActivity().getApplicationContext(), "Stop Recording",
                             Toast.LENGTH_SHORT).show();
+                    rec_mic.onPause();
+                    rec_acc.onPause();
                 }
             }
 
@@ -193,6 +281,8 @@ public class TabFragment2 extends Fragment {
                 if(toggleRealTime.isChecked()){
                     Toast.makeText(getActivity().getApplicationContext(), "Record",
                             Toast.LENGTH_SHORT).show();
+                    rec_mic.run();
+                    rec_acc.run();
                 }
                 else
                 {
@@ -218,6 +308,9 @@ public class TabFragment2 extends Fragment {
                 Toast.makeText(getActivity().getApplicationContext(), getString(R.string.tab2_title1),
                         Toast.LENGTH_SHORT).show();
                 textTitle.setText(getString(R.string.tab2_title1));
+                recordButton.setEnabled(true);
+                playButton.setEnabled(false);
+                pauseButton.setEnabled(false);
             }
         });
 
@@ -230,6 +323,9 @@ public class TabFragment2 extends Fragment {
                 Toast.makeText(getActivity().getApplicationContext(), getString(R.string.tab2_title2),
                         Toast.LENGTH_SHORT).show();
                 textTitle.setText(getString(R.string.tab2_title2));
+                recordButton.setEnabled(false);
+                playButton.setEnabled(true);
+                pauseButton.setEnabled(true);
             }
         });
     }
@@ -242,24 +338,32 @@ public class TabFragment2 extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
+        rec_acc.onPause();
+        rec_mic.onPause();
 
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-
         // Is fragment currently visible ?
         if (this.isVisible()) {
             // Is it becoming invisible?
             if (!isVisibleToUser) {
-
+                rec_acc.onPause();
+                rec_mic.onPause();
+                graph.removeAllSeries();
             }
-        }
-        else { // Is fragment currently invisible?
-            // Is it becoming visible?
-            if (isVisibleToUser) {
-
+            else if(graph != null) {
+                Xlo.isEnabled = Tab1Data.getBool("Vibration", true);
+                MicData.isEnabled = Tab1Data.getBool("Noise", false);
+                if (MicData.isEnabled)
+                    graph.addSeries(audioSeries);
+                if (Xlo.isEnabled) {
+                    graph.addSeries(xSeries);
+                    graph.addSeries(ySeries);
+                    graph.addSeries(zSeries);
+                }
             }
         }
     }
