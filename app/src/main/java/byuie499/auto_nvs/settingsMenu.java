d2584eb8a6e1695f82c00fd73a0bfffc0ff22aa8
Matt <matt.tomlinson1@gmail.com>
2016-06-09 19:33:23 -0600

    Cleaned up settings menu and added last few options

--- app/src/main/java/byuie499/auto_nvs/settingsMenu.java
+++ app/src/main/java/byuie499/auto_nvs/settingsMenu.java
@@ -2,8 +2,6 @@ package byuie499.auto_nvs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
