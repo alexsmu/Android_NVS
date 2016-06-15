9d0e4b12c4aa06d2aa38c253027ac53e19eea6b8
Matt <matt.tomlinson1@gmail.com>
2016-06-15 13:53:53 -0600

    Settings and current profile are now remembered and retrieved.

--- app/src/main/java/byuie499/auto_nvs/settingsMenu.java
+++ app/src/main/java/byuie499/auto_nvs/settingsMenu.java
@@ -1,5 +1,6 @@
 package byuie499.auto_nvs;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -18,10 +19,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsMenu extends AppCompatActivity implements View.OnFocusChangeListener{
+public class SettingsMenu extends AppCompatActivity {
     private SettingsData staticData = null;
     private Spinner fileSpinner;
     private SharedPreferences prefs;
+    private SharedPreferences settingsPrefs;
     private EditText ratio1;
     private EditText ratio2;
     private EditText ratio3;
@@ -42,6 +44,7 @@ public class SettingsMenu extends AppCompatActivity implements View.OnFocusChang
         super.onCreate(savedInstanceState);
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        settingsPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = prefs.edit();
         editor.apply();
 
@@ -63,64 +66,132 @@ public class SettingsMenu extends AppCompatActivity implements View.OnFocusChang
         adapter.setDropDownViewResource(R.layout.spinner_item);
         fileSpinner.setAdapter(adapter);
 
+        settingsPrefs = getSharedPreferences(getString(R.string.preference_file), Context.MODE_PRIVATE);
+
+        setSpinner();
+
         addListenerToSpinner();
-        addListenerToNames();
-        addListenersToValues();
         addListenersToCheckBoxes();
         addListenerToButtons();
+
+        update();
+    }
+
+    void update(){
+        ratio1  = (EditText) findViewById((R.id.value1));
+        ratio2  = (EditText) findViewById((R.id.value2));
+        ratio3  = (EditText) findViewById((R.id.value3));
+        ratio4  = (EditText) findViewById((R.id.value4));
+        ratio5  = (EditText) findViewById((R.id.value5));
+        ratio6  = (EditText) findViewById((R.id.value6));
+
+        name2  = (EditText) findViewById((R.id.name2));
+        name3  = (EditText) findViewById((R.id.name3));
+        name4  = (EditText) findViewById((R.id.name4));
+        name5  = (EditText) findViewById((R.id.name5));
+        name6  = (EditText) findViewById((R.id.name6));
+
+        ratio7  = (EditText) findViewById((R.id.tire1));
+        ratio8  = (EditText) findViewById((R.id.gearval));
+
+        saveVars();
+
+        String profile = fileSpinner.getSelectedItem().toString();
+        prefs = getSharedPreferences(profile, MODE_PRIVATE);
+        settingsPrefs.edit().putString("profile", profile).apply();
+        //Toast.makeText(getApplicationContext(), profile, Toast.LENGTH_SHORT).show();
+
+        loadVars();
+    }
+
+    void saveVars(){
+        prefs.edit().putString("ratio1", ratio1.getText().toString()).apply();
+        prefs.edit().putString("ratio2", ratio2.getText().toString()).apply();
+        prefs.edit().putString("ratio3", ratio3.getText().toString()).apply();
+        prefs.edit().putString("ratio4", ratio4.getText().toString()).apply();
+        prefs.edit().putString("ratio5", ratio5.getText().toString()).apply();
+        prefs.edit().putString("ratio6", ratio6.getText().toString()).apply();
+        prefs.edit().putString("name2", name2.getText().toString()).apply();
+        prefs.edit().putString("name3", name3.getText().toString()).apply();
+        prefs.edit().putString("name4", name4.getText().toString()).apply();
+        prefs.edit().putString("name5", name5.getText().toString()).apply();
+        prefs.edit().putString("name6", name6.getText().toString()).apply();
+        prefs.edit().putString("ratio7", ratio7.getText().toString()).apply();
+        prefs.edit().putString("ratio8", ratio8.getText().toString()).apply();
+    }
+
+    void loadVars(){
+        ratio1.setText(prefs.getString("ratio1", ""));
+        ratio2.setText(prefs.getString("ratio2", ""));
+        ratio3.setText(prefs.getString("ratio3", ""));
+        ratio4.setText(prefs.getString("ratio4", ""));
+        ratio5.setText(prefs.getString("ratio5", ""));
+        ratio6.setText(prefs.getString("ratio6", ""));
+        name2.setText(prefs.getString("name2", ""));
+        name3.setText(prefs.getString("name3", ""));
+        name4.setText(prefs.getString("name4", ""));
+        name5.setText(prefs.getString("name5", ""));
+        name6.setText(prefs.getString("name6", ""));
+        ratio7.setText(prefs.getString("ratio7", ""));
+        ratio8.setText(prefs.getString("ratio8", ""));
+
+        //Toast.makeText(getApplicationContext(), "Loaded", Toast.LENGTH_SHORT).show();
+    }
+
+    void setSpinner(){
+        String profile = settingsPrefs.getString("profile", "");
+
+        switch(profile) {
+            case "Profile 1":
+                Toast.makeText(getApplicationContext(), profile, Toast.LENGTH_SHORT).show();
+                fileSpinner.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fileSpinner.setSelection(0);
+                    }
+                });
+                update();
+                break;
+            case "Profile 2":
+                Toast.makeText(getApplicationContext(), profile, Toast.LENGTH_SHORT).show();
+                fileSpinner.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fileSpinner.setSelection(1);
+                    }
+                });
+                update();
+                break;
+            case "Profile 3":
+                Toast.makeText(getApplicationContext(), profile, Toast.LENGTH_SHORT).show();
+                fileSpinner.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fileSpinner.setSelection(2);
+                    }
+                });
+                update();
+                break;
+            case "Profile 4":
+                Toast.makeText(getApplicationContext(), profile, Toast.LENGTH_SHORT).show();
+                fileSpinner.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fileSpinner.setSelection(3);
+                    }
+                });
+                update();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     void addListenerToSpinner(){
         fileSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                ratio1  = (EditText) findViewById((R.id.value1));
-                ratio2  = (EditText) findViewById((R.id.value2));
-                ratio3  = (EditText) findViewById((R.id.value3));
-                ratio4  = (EditText) findViewById((R.id.value4));
-                ratio5  = (EditText) findViewById((R.id.value5));
-                ratio6  = (EditText) findViewById((R.id.value6));
-
-                name2  = (EditText) findViewById((R.id.name2));
-                name3  = (EditText) findViewById((R.id.name3));
-                name4  = (EditText) findViewById((R.id.name4));
-                name5  = (EditText) findViewById((R.id.name5));
-                name6  = (EditText) findViewById((R.id.name6));
-
-                ratio7  = (EditText) findViewById((R.id.tire1));
-                ratio8  = (EditText) findViewById((R.id.gearval));
-
-                prefs.edit().putString("ratio1", ratio1.getText().toString()).apply();
-                prefs.edit().putString("ratio2", ratio2.getText().toString()).apply();
-                prefs.edit().putString("ratio3", ratio3.getText().toString()).apply();
-                prefs.edit().putString("ratio4", ratio4.getText().toString()).apply();
-                prefs.edit().putString("ratio5", ratio5.getText().toString()).apply();
-                prefs.edit().putString("ratio6", ratio6.getText().toString()).apply();
-                prefs.edit().putString("name2", name2.getText().toString()).apply();
-                prefs.edit().putString("name3", name3.getText().toString()).apply();
-                prefs.edit().putString("name4", name4.getText().toString()).apply();
-                prefs.edit().putString("name5", name5.getText().toString()).apply();
-                prefs.edit().putString("name6", name6.getText().toString()).apply();
-                prefs.edit().putString("ratio7", ratio7.getText().toString()).apply();
-                prefs.edit().putString("ratio8", ratio8.getText().toString()).apply();
-
-                String text = fileSpinner.getSelectedItem().toString();
-                prefs = getSharedPreferences(text, MODE_PRIVATE);
-
-                ratio1.setText(prefs.getString("ratio1", ""));
-                ratio2.setText(prefs.getString("ratio2", ""));
-                ratio3.setText(prefs.getString("ratio3", ""));
-                ratio4.setText(prefs.getString("ratio4", ""));
-                ratio5.setText(prefs.getString("ratio5", ""));
-                ratio6.setText(prefs.getString("ratio6", ""));
-
-                name2.setText(prefs.getString("name2", ""));
-                name3.setText(prefs.getString("name3", ""));
-                name4.setText(prefs.getString("name4", ""));
-                name5.setText(prefs.getString("name5", ""));
-                name6.setText(prefs.getString("name6", ""));
-
-                ratio7.setText(prefs.getString("ratio7", ""));
-                ratio8.setText(prefs.getString("ratio8", ""));
+                update();
             }
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -135,17 +206,8 @@ public class SettingsMenu extends AppCompatActivity implements View.OnFocusChang
             save1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    prefs.edit().putString("ratio1", ratio1.getText().toString()).apply();
-                    prefs.edit().putString("ratio2", ratio2.getText().toString()).apply();
-                    prefs.edit().putString("ratio3", ratio3.getText().toString()).apply();
-                    prefs.edit().putString("ratio4", ratio4.getText().toString()).apply();
-                    prefs.edit().putString("ratio5", ratio5.getText().toString()).apply();
-                    prefs.edit().putString("ratio6", ratio6.getText().toString()).apply();
-                    prefs.edit().putString("name2", name2.getText().toString()).apply();
-                    prefs.edit().putString("name3", name3.getText().toString()).apply();
-                    prefs.edit().putString("name4", name4.getText().toString()).apply();
-                    prefs.edit().putString("name5", name5.getText().toString()).apply();
-                    prefs.edit().putString("name6", name6.getText().toString()).apply();
+                    saveVars();
+                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -153,65 +215,10 @@ public class SettingsMenu extends AppCompatActivity implements View.OnFocusChang
             save2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    prefs.edit().putString("ratio7", ratio7.getText().toString()).apply();
-                    prefs.edit().putString("ratio8", ratio8.getText().toString()).apply();
+                    saveVars();
+                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
                 }
             });
-        }
-    }
-
-    void addListenerToNames(){
-
-        TextView name1 = (TextView) findViewById((R.id.name1));
-        if (name1 != null) {
-            name1.setOnFocusChangeListener(this);
-        }
-        EditText name2 = (EditText) findViewById((R.id.name2));
-        if (name2 != null) {
-            name2.setOnFocusChangeListener(this);
-        }
-        EditText name3 = (EditText) findViewById((R.id.name3));
-        if (name3 != null) {
-            name3.setOnFocusChangeListener(this);
-        }
-        EditText name4 = (EditText) findViewById((R.id.name4));
-        if (name4 != null) {
-            name4.setOnFocusChangeListener(this);
-        }
-        EditText name5 = (EditText) findViewById((R.id.name5));
-        if (name5 != null) {
-            name5.setOnFocusChangeListener(this);
-        }
-        EditText name6 = (EditText) findViewById((R.id.name6));
-        if (name6 != null) {
-            name6.setOnFocusChangeListener(this);
-        }
-
-    }
-
-    void addListenersToValues(){
-        if (ratio1 != null) {
-            ratio1.setOnFocusChangeListener(this);
-        }
-        EditText ratio2 = (EditText) findViewById((R.id.value2));
-        if (ratio2 != null) {
-            ratio2.setOnFocusChangeListener(this);
-        }
-        EditText ratio3 = (EditText) findViewById((R.id.value3));
-        if (ratio3 != null) {
-            ratio3.setOnFocusChangeListener(this);
-        }
-        EditText ratio4 = (EditText) findViewById((R.id.value4));
-        if (ratio4 != null) {
-            ratio4.setOnFocusChangeListener(this);
-        }
-        EditText ratio5 = (EditText) findViewById((R.id.value5));
-        if (ratio5 != null) {
-            ratio5.setOnFocusChangeListener(this);
-        }
-        EditText ratio6 = (EditText) findViewById((R.id.value6));
-        if (ratio6 != null) {
-            ratio6.setOnFocusChangeListener(this);
         }
     }
 
@@ -291,26 +298,15 @@ public class SettingsMenu extends AppCompatActivity implements View.OnFocusChang
     }
 
     @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        switch (v.getId()) {
-            case R.id.name1:
-                break;
-            default:
-                //Toast.makeText(getApplicationContext(), "Focus changed", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
-
-    @Override
     public void onPause() {
         super.onPause();
-
+        saveVars();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
+        update();
     }
 
 }
