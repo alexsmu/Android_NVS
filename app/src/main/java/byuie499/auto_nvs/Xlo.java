6690259289da41b8f024052f630d095f47083126
alexsmu <alexsmu@gmail.com>
2016-07-12 17:51:23 -0600

    Changes to attempt to improve accelerometer accuracy: -added code for radius coordinate (from spherical coordinates) -average accelerometer samples over time before timer triggers precise reading -removing average from sample -decreased sampling rate to 250

--- app/src/main/java/byuie499/auto_nvs/Xlo.java
+++ app/src/main/java/byuie499/auto_nvs/Xlo.java
@@ -9,6 +9,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.util.Timer;
@@ -20,14 +21,34 @@ public class Xlo {
     public static double xc = 0;  // current value as read from the sensor
     public static double yc = 0;
     public static double zc = 0;
+    public static double rc = 0; // current radius (polar coordinates)
+    public static double totalX = 0;  //
+    public static double totalY = 0;
+    public static double totalZ = 0;
+    public static double totalR = 0;
+    public static double avgX = 0;  //
+    public static double avgY = 0;
+    public static double avgZ = 0;
+    public static double avgR = 0;
+    public static long current_time;
+    public static long last_time;
     public static double[] xAcc; // accumulator for values
     public static double[] yAcc;
     public static double[] zAcc;
+    public static double[] rAcc;
     public static double[] xAcc0; // accumulator for values
     public static double[] yAcc0;
     public static double[] zAcc0;
+    public static double[] rAcc0;
+    public static boolean fftInprogress;
     public static boolean isRunning = false; // continue thread flag
     public static boolean isEnabled = true;  // vibration selected flag
+    private boolean isSampling = false;
+    private boolean sensorEvent = false;
+    private int readCount = 0;
+    public static double avg_sample_Ts = 0;
+    private double avg_sampling = 0;
+
     private int val = 0; // index for accumulator
     private int N = 0;   // number of samples to accumulate before overwriting
     private int accum = 1; // divisions of buffer to wait before sending message
@@ -43,21 +64,23 @@ public class Xlo {
         xAcc = new double[samples];
         yAcc = new double[samples];
         zAcc = new double[samples];
+        rAcc = new double[samples];
         xAcc0 = new double[samples];
         yAcc0 = new double[samples];
         zAcc0 = new double[samples];
+        rAcc0 = new double[samples];
         sm = (SensorManager) mMain.getSystemService(Activity.SENSOR_SERVICE);
 
         /*Check for Linear Acceleration Sensor*/
         /*If no Linear Accelerometer is present assign Accelerometer*/
-        if (sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null){
-            Log.d("Linear Accelerometer: ", "present");
-            accelerometer = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        }
-        else {
-            Log.d("Linear Accelerometer: ", "not present");
+//        if (sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null){
+//            Log.d("Linear Accelerometer: ", "present");
+//            accelerometer = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+//        }
+//        else {
+//            Log.d("Linear Accelerometer: ", "not present");
             accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        }
+//        }
     }
 
     public void run() {
@@ -70,24 +93,35 @@ public class Xlo {
             accumulate = new TimerTask() {
                 @Override
                 public synchronized void run() {
-                    xAcc0[val] = xc; // store current sensor values
-                    yAcc0[val] = yc;
-                    zAcc0[val] = zc;
+                    isSampling = true;
+                    last_time = current_time;
+                    current_time = System.currentTimeMillis();
+                    avg_sampling += (current_time - last_time);
+                    while(sensorEvent);
+                    xAcc0[val] = xc - avgX; // store current sensor values
+                    yAcc0[val] = yc - avgY;
+                    zAcc0[val] = zc - avgZ;
+                    rAcc0[val] = rc - avgR;
                     val = (val + 1) % N; // increment index
                     if (val % (N / accum) == 0) { // send message to main thread
+                        avg_sample_Ts = avg_sampling / (N / accum);
+                        avg_sampling = 0;
                         for (int i = 0; i < N; ++i) {
                             xAcc[i] = xAcc0[i]; // copy current sensor values
                             yAcc[i] = yAcc0[i];
                             zAcc[i] = zAcc0[i];
+                            rAcc[i] = rAcc0[i];
                         }
                         Message done = mHandler.obtainMessage(3);
                         mHandler.sendMessage(done);
                     }
+                    isSampling = false;
                 }
             };
+            current_time = System.currentTimeMillis();
             timer.schedule(accumulate, // timer task
                     0, // delay
-                    1); // period in ms
+                    4); // period in ms
         }
     }
 
@@ -103,9 +137,29 @@ public class Xlo {
     public SensorEventListener xlo_read = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            xc = event.values[0];
-            yc = event.values[1];
-            zc = event.values[2];
+            if (!isSampling) {
+                sensorEvent = true;
+                xc = event.values[0];
+                yc = event.values[1];
+                zc = event.values[2];
+                rc = Math.sqrt(xc * xc + yc * yc + zc * zc);
+                totalX += xc;
+                totalY += yc;
+                totalZ += zc;
+                totalR += rc;
+                readCount++;
+                avgX = totalX / readCount;
+                avgY = totalY / readCount;
+                avgZ = totalZ / readCount;
+                avgR = totalR / readCount;
+                sensorEvent = false;
+            } else {
+                readCount = 0;
+                totalX = 0;  //
+                totalY = 0;
+                totalZ = 0;
+                totalR = 0;
+            }
         }
 
         @Override
